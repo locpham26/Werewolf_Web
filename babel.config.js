e0f0ac85c444bcbd8347d9179306242117451603
ltta18 <thuylinh7720@gmail.com>
2020-11-02 00:26:32 +0700

    Add scss base file

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,11 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  css: {
+    loaderOptions: {
+      sass: {
+        data: '@import "@/assets/styles/_variables',
+      },
+    },
+  },
 };
--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style lang="scss">
+@import '@/assets/styles/_base';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
